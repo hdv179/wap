adce3e0ed904ba756356f552624843cdf2d231ca
hdv179 <43834407+hdv179@users.noreply.github.com>
2026-08-08 00:47:10 +0700

    Update header.js

--- header.js
+++ header.js
@@ -10,7 +10,7 @@ document.write('<div class="title-head title-head--center"><a href="index.html" 
 '<input type="text" name="q" placeholder="Nhập tên game/bài viết..." class="form-control" required="required" style="width: 100%; box-sizing: border-box; -webkit-box-sizing: border-box; height: 30px; margin: 0; vertical-align: middle;" />' +
 '</div>' +
 '<div style="display: table-cell; vertical-align: middle; width: 1%; white-space: nowrap;">' +
-'<button type="submit" class="btn btn--small" style="height: 30px; line-height: 1; margin: 0; vertical-align: middle; white-space: nowrap;">Tìm kiếm</button>' +
+'<button type="submit" class="btn btn--small" style="line-height: 1; margin: 0; vertical-align: middle; white-space: nowrap;">Tìm kiếm</button>' +
 '</div>' +
 '</form>' +
 '</div>');
