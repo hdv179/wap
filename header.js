01138d97a3cd58e52d5dad80314388d20dd4436c
hdv179 <43834407+hdv179@users.noreply.github.com>
2026-08-07 22:41:16 +0700

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+document.write('<div class="title-head title-head--center">HDV179 WAP</div>' +
+'<div class="nav-menu nav-menu--center">' +
+'<a href="index.html">Trang Chủ</a> | ' +
+'<a href="category.html?cat=gameloft">Gameloft</a> | ' +
+'<a href="category.html?cat=teamobi">TeaMobi</a> | ' +
+'<a href="category.html?cat=ungdung">Ứng Dụng</a>' +
+'</div>' +
+'<div class="wap-card wap-card--compact">' +
+'<form action="category.html" method="GET" class="search-form">' +
+'<input type="text" name="q" placeholder="Nhập tên game/bài viết..." class="form-control form-control--grow" required="required" />' +
+'<button type="submit" class="btn btn--small">Tìm kiếm</button>' +
+'</form>' +
+'</div>');
