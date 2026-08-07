21070dd760541f178f2bdf1794d7bf244dbe243b
hdv179 <43834407+hdv179@users.noreply.github.com>
2026-08-08 00:47:56 +0700

    Update header.js

--- header.js
+++ header.js
@@ -7,7 +7,7 @@ document.write('<div class="title-head title-head--center"><a href="index.html" 
 '<div class="wap-card wap-card--compact">' +
 '<form action="category.html" method="GET" class="search-form" style="display: table; width: 100%; border-collapse: collapse;">' +
 '<div style="display: table-cell; vertical-align: middle; padding-right: 4px;">' +
-'<input type="text" name="q" placeholder="Nhập tên game/bài viết..." class="form-control" required="required" style="width: 100%; box-sizing: border-box; -webkit-box-sizing: border-box; height: 30px; margin: 0; vertical-align: middle;" />' +
+'<input type="text" name="q" placeholder="Nhập tên game/bài viết..." class="form-control" required="required" style="width: 100%; box-sizing: border-box; -webkit-box-sizing: border-box; margin: 0; vertical-align: middle;" />' +
 '</div>' +
 '<div style="display: table-cell; vertical-align: middle; width: 1%; white-space: nowrap;">' +
 '<button type="submit" class="btn btn--small" style="line-height: 1; margin: 0; vertical-align: middle; white-space: nowrap;">Tìm kiếm</button>' +
