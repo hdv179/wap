bd866f094912ac686a4442a7ff25888ce0cef235
hdv179 <43834407+hdv179@users.noreply.github.com>
2026-08-07 22:59:32 +0700

    Create gangstar-rio.js

---
+++ data/items/gangstar-rio.js
@@ -0,0 +1,20 @@
+window.CURRENT_ITEM_DATA = {
+  "id": "gangstar-rio",
+  "title": "Gangstar Rio: City of Saints",
+  "vendor": "Gameloft",
+  "screen": "240x320",
+  "version": "1.0.2",
+  "date": "2026-01-01",
+  "blocks": [
+    { "type": "text", "value": "Kham phá thanh pho Rio de Janeiro voi nhieu nhiem vu hap dan." },
+    { "type": "image", "value": "assets/images/gangstar-screen.png", "caption": "Anh trong game" }
+  ],
+  "downloads": [
+    {
+      "groupTitle": "Phien ban Java (.jar)",
+      "files": [
+        { "label": "Man hinh 240x320 (JAR)", "url": "downloads/gangstar_240x320.jar" }
+      ]
+    }
+  ]
+};
