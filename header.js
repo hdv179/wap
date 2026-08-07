b8989e1077267ac1547d3537d959636c9fd03fa7
hdv179 <43834407+hdv179@users.noreply.github.com>
2026-08-08 00:43:57 +0700

    Update header.js

--- header.js
+++ header.js
@@ -5,8 +5,12 @@ document.write('<div class="title-head title-head--center"><a href="index.html" 
 '<a href="category.html?cat=ungdung">Ứng Dụng</a>' +
 '</div>' +
 '<div class="wap-card wap-card--compact">' +
-'<form action="category.html" method="GET" class="search-form" style="display: flex; align-items: center; gap: 4px;">' +
-'<input type="text" name="q" placeholder="Nhập tên game/bài viết..." class="form-control form-control--grow" required="required" style="flex: 1;" />' +
-'<button type="submit" class="btn btn--small" style="white-space: nowrap;">Tìm kiếm</button>' +
+'<form action="category.html" method="GET" class="search-form" style="display: table; width: 100%; border-collapse: collapse;">' +
+'<div style="display: table-cell; vertical-align: middle; padding-right: 4px;">' +
+'<input type="text" name="q" placeholder="Nhập tên game/bài viết..." class="form-control" required="required" style="width: 100%; box-sizing: border-box; -webkit-box-sizing: border-box; height: 30px; margin: 0; vertical-align: middle;" />' +
+'</div>' +
+'<div style="display: table-cell; vertical-align: middle; width: 1%; white-space: nowrap;">' +
+'<button type="submit" class="btn btn--small" style="height: 30px; line-height: 1; margin: 0; vertical-align: middle; white-space: nowrap;">Tìm kiếm</button>' +
+'</div>' +
 '</form>' +
 '</div>');
