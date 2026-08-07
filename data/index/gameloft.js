281516bc69e1d740b80df1441290882eb8a0af00
hdv179 <43834407+hdv179@users.noreply.github.com>
2026-08-07 22:59:00 +0700

    Create gameloft.js

---
+++ data/index/gameloft.js
@@ -0,0 +1,16 @@
+window.CURRENT_CAT_DATA = [
+  {
+    "id": "gangstar-rio",
+    "title": "Gangstar Rio: City of Saints",
+    "thumb": "assets/images/gangstar.png",
+    "screen": "240x320",
+    "vendor": "Gameloft"
+  },
+  {
+    "id": "asphalt-6",
+    "title": "Asphalt 6: Adrenaline",
+    "thumb": "assets/images/asphalt.png",
+    "screen": "240x320",
+    "vendor": "Gameloft"
+  }
+];
